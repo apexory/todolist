4a38518337b6219e48b11172f9d118198d1b7cb3
Ivan <138053343+apexory@users.noreply.github.com>
2024-06-26 23:04:34 +0300

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,167 @@
+import { useState, useEffect } from 'react';
+import { io } from 'socket.io-client'; 
+import * as config from 'config-todolist'; 
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import LaptopIcon from '@mui/icons-material/Laptop';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
+import Avatar from '@mui/material/Avatar';
+
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
+import GitHubIcon from '@mui/icons-material/GitHub';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const socket = io(config.webSocketsAddress)
+
+const darkTheme = createTheme({ palette: { mode: 'dark' } });
+const lightTheme = createTheme({ palette: { mode: 'light' } });
 
 function App() {
+  const [theme, setTheme] = useState(darkTheme)
+  const changeTheme = () => setTheme((theme == darkTheme) ? lightTheme : darkTheme)
+
+  const [plannedTasks, setPlannedTasks] = useState([])
+  const [makingTasks, setMakingTasks] = useState([])
+  const [completedTasks, setCompletedTasks] = useState([])
+
+  const [notes, setNotes] = useState('')
+  const [notesSavings, setNotesSavings] = useState(0)
+
+  useEffect(() => {
+    socket.on('getTasks', (data) => {
+      setPlannedTasks(data.planned)
+      setMakingTasks(data.making)
+      setCompletedTasks(data.completed)
+      setNotes(data.notes)
+    });
+  }, [])
+
+  const addTask = (event, type) => {
+    if(event.target.value == '' || event.key != 'Enter') return;
+    if(type == 'planned') setPlannedTasks([...plannedTasks, event.target.value])
+    if(type == 'making') setMakingTasks([...makingTasks, event.target.value])
+    if(type == 'completed') setCompletedTasks([...completedTasks, event.target.value])
+    socket.emit('addTask', type, event.target.value)
+    event.target.value = ''
+  }
+
+  const deleteTask = (type, index) => {
+    if(type == 'planned') setPlannedTasks(plannedTasks.filter((_, i) => i != index))
+    if(type == 'making') setMakingTasks(makingTasks.filter((_, i) => i != index))
+    if(type == 'completed') setCompletedTasks(completedTasks.filter((_, i) => i != index))
+    socket.emit('deleteTask', type, index)
+  }
+
+  const saveNote = (event) => {
+    setNotesSavings(notesSavings + 1)
+    setNotes(event.target.value)
+    if(notesSavings >= 5) {
+      socket.emit('saveNote', event.target.value)
+      setNotesSavings(0)
+    }
+  }
+
   return (
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
     <div style={{ fontFamily: 'roboto' }} className="App">
 
       <Stack alignItems="center" gap="10px" sx={{ marginTop: '70px' }}>
-          <LaptopIcon sx={{ fontSize: 50 }} />
+          <Avatar sx={{ height: '60px', width: '60px' }} src="https://avatars.githubusercontent.com/u/138053343?v=4" />
           <Typography sx={{ fontSize: 20, height: '20px' }} >Todo-List App</Typography>
-          <Typography sx={{ fontSize: 15, color: '#383838' }} >by apexory</Typography>
+          <Typography color="text.secondary" sx={{ fontSize: 15 }} >by apexory</Typography>
       </Stack>
 
-      <Stack direction="row" gap="30px" justifyContent="center" sx={{ marginTop: '30px' }}>
-        <div>efwef</div>
-        <div>efwef</div>
-        <div>efwef</div>
+      <Stack direction="row" gap="10px" justifyContent="center" sx={{ marginTop: '30px' }}>
+        <Card variant="outlined" sx={{ minWidth: 265 }}>
+          <CardContent>
+            <Stack gap="10px">
+              <Typography variant="h5" sx={{ textAlign: 'center' }}>Planned</Typography>
+              <TextField onKeyDown={(event) => addTask(event, 'planned')} variant="outlined" size="small" label="Add task ..." />
+              <Stack style={{ overflow: 'auto', height: '200px' }} gap="8px" mt="10px">
+                {plannedTasks.map((task, i) => (
+                  <ListItem key={i} sx={{ borderWidth: '1px', borderColor: `text.disabled`, borderStyle: 'solid', borderRadius: '5px' }} secondaryAction={<IconButton onClick={() => deleteTask('planned', i)} edge="end"><DeleteIcon /></IconButton>} disablePadding><ListItemButton><ListItemText primary={ task } /></ListItemButton></ListItem>
+                ))}
+              </Stack>
+            </Stack>
+          </CardContent>
+        </Card>
+
+
+        <Card variant="outlined" sx={{ minWidth: 265 }}>
+          <CardContent>
+            <Stack gap="10px">
+              <Typography variant="h5" sx={{ textAlign: 'center' }}>Making</Typography>
+              <TextField onKeyDown={(event) => addTask(event, 'making')} variant="outlined" size="small" label="Add task ..." />
+              <Stack style={{ overflow: 'auto', height: '200px' }} gap="8px" mt="10px">
+                {makingTasks.map((task, i) => (
+                  <ListItem key={i} sx={{ borderWidth: '1px', borderColor: `text.disabled`, borderStyle: 'solid', borderRadius: '5px' }} secondaryAction={<IconButton onClick={() => deleteTask('making', i)} edge="end"><DeleteIcon /></IconButton>} disablePadding><ListItemButton><ListItemText primary={ task } /></ListItemButton></ListItem>
+                ))}
+              </Stack>
+            </Stack>
+          </CardContent>
+        </Card>
+
+        <Card variant="outlined" sx={{ minWidth: 265 }}>
+          <CardContent>
+            <Stack gap="10px">
+              <Typography variant="h5" sx={{ textAlign: 'center' }}>Completed</Typography>
+              <TextField onKeyDown={(event) => addTask(event, 'completed')} variant="outlined" size="small" label="Add task ..." />
+              <Stack style={{ overflow: 'auto', height: '200px' }} gap="8px" mt="10px">
+                {completedTasks.map((task, i) => (
+                  <ListItem key={i} sx={{ borderWidth: '1px', borderColor: `text.disabled`, borderStyle: 'solid', borderRadius: '5px' }} secondaryAction={<IconButton onClick={() => deleteTask('completed', i)} edge="end"><DeleteIcon /></IconButton>} disablePadding><ListItemButton><ListItemText primary={ task } /></ListItemButton></ListItem>
+                ))}
+              </Stack>
+            </Stack>
+          </CardContent>
+        </Card>
+
+        <Card variant="outlined" sx={{ minWidth: 230, height: '345px' }}>
+          <CardContent>
+            <Stack gap="10px">
+              <Typography variant="h5" sx={{ textAlign: 'center' }}>Notes</Typography>
+              <Stack style={{ overflow: 'auto', height: '260px' }} gap="8px">
+                <TextField onChange={ saveNote } label="e.x., the plans for today" multiline minRows={10} variant="filled" value={ notes } />
+              </Stack>
+            </Stack>
+          </CardContent>
+        </Card>
       </Stack>
 
       <div>
-        <div style={{ position: 'absolute', bottom: '10px', left: '10px' }}>
-          ewfewfeewfwfe
+        <div style={{ display: 'flex', alignItems: 'center', gap: '10px', position: 'absolute', bottom: '15px', left: '15px' }}>
+          <Button size="small" color="inherit" variant="outlined">by apexory</Button>
         </div>
 
-        <div style={{ position: 'absolute', bottom: '10px', right: '10px' }}>
-          olalallalallalal
+        <div style={{ display: 'flex', alignItems: 'center', gap: '10px', position: 'absolute', bottom: '15px', right: '15px' }}>
+          <Button startIcon={<GitHubIcon />} size="small" color="inherit" variant="outlined">my github</Button>
+        </div>
+
+        <div style={{ display: 'flex', alignItems: 'center', gap: '10px', position: 'absolute', top: '10px', right: '10px' }}>
+          <IconButton onClick={ changeTheme } color="inherit">{ theme == darkTheme ? <Brightness7Icon /> : <Brightness4Icon />}</IconButton>
         </div>
       </div>
 
     </div>
+    </ThemeProvider>
   );
 }
 
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,32 @@
 import express, { Express, Request, Response } from "express";
+import fs from "fs";
 
 const app: Express = express();
 const port: number = 3001;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('hi!')
-})
+const server = app.listen(port, () => console.log('[server]: Server is running. Good!'))
 
-app.listen(port, () => console.log('[server]: Server is running. Good!'))
+import { Server } from "socket.io";
+const io = new Server(server, { cors: { origin: '*' } });
+
+io.on('connection', (socket) => {
+    let data = JSON.parse(fs.readFileSync('../data.json', { encoding: 'utf8' }))
+    console.log('[client]: connected!')
+    socket.emit('getTasks', data)
+    socket.on('addTask', (type, value) => {
+        if(type == 'planned') data.planned.push(value)
+        if(type == 'making') data.making.push(value)
+        if(type == 'completed') data.completed.push(value)
+        fs.writeFileSync('../data.json', JSON.stringify(data))
+    })
+    socket.on('deleteTask', (type, index) => {
+        if(type == 'planned') data.planned = data.planned.filter((_: null, i: number) => i != index)
+        if(type == 'making') data.making = data.making.filter((_: null, i: number) => i != index)
+        if(type == 'completed') data.completed = data.completed.filter((_: null, i: number) => i != index)
+        fs.writeFileSync('../data.json', JSON.stringify(data))
+    })
+    socket.on('saveNote', (value) => {
+        data.notes = value
+        fs.writeFileSync('../data.json', JSON.stringify(data))
+    })
+})
